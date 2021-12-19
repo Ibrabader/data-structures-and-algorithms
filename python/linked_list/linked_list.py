3cac07242fe9aeaf1ecce03363fe9b131ff88da8
Ibrahim Bader <ebraheem1996@hotmail.com>
2021-12-19 21:01:19 +0200

    solved

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,29 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+# class LinkedList:
+#     """
+#     Put docstring here
+#     """
 
-    def __init__(self):
-        # initialization here
-        pass
+#     def __init__(self):
+#         # initialization here
+#         pass
 
-    def some_method(self):
-        # method body here
-        pass
+#     def some_method(self):
+#         # method body here
+#         pass
+
+import math 
+
+def claculat(x,y):
+   
+    a=x**2
+    b=y**2
+    z=math.sqrt(a+b)
+
+    return z
+
+print(claculat(50,60))
+
+def claculat2(hight,rob,alt):
+     
+
+
